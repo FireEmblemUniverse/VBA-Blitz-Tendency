7e03ff86fd1d1ed8e0c56b7b2369914e16f52408
We-Who-Belong <naisurte@gmail.com>
2018-07-09 15:32:09 -0700

    [Untested] Axefaith accomodations
    Nothing has been tested! I just have to get up now.
    
    -Modifed chapter loot to allow Thief contribute to combat;
    ->Decided to give the other two melee monsterkillers because they're cool things ~'3'~
    -Added newlines to ends of all modified files. A lot of them in some cases. :)

--- Engine Hacks/Skill System/Blade Skills/AxeFaith.s
+++ Engine Hacks/Skill System/Blade Skills/AxeFaith.s
@@ -15,24 +15,23 @@ beq 	NoSkill
 @Unit has skill, check to see if unit has an axe equipped
 mov     r0, #0x50      @Move to the attacking unit weapon type.
 ldrb    r0, [r4, r0]   @Load the attacking unit weapon type.
-ldr     r1,            @Load in the list of Faire Skills.
-ldrb    r1,[r1,r0]     @Load in the Faire Skill corresponding to the equipped weapon.
-mov     r0,r4          @Store attacker data into r0 (for the purposes of SkillTester).
-ldr     r3,SkillTester 
-mov     lr, r3
-.short 0xf800         @Call Skill Tester.
-cmp r0, #0            @Check if unit has the corresponding skill.
+cmp     r0, #2         @Is it Axe?
+bne     NoSkill        @If not, goto end
+ldr     r3,SkillTester @If it is,
+mov     lr, r3         
+.short 0xf800          @Call Skill Tester.
+cmp r0, #0             @Check if unit has the corresponding skill.
 beq     NoSkill
 
 @Add to Hit
-mov     r0, 0x19
-ldrh    r0, [r4,r0]   @load luck
-lsr     r1, r0, #1    @put half luck in r1
-add     r0, r0, r1    @add back together.
+mov     r0, #0x19
+ldrh    r0, [r4,r0]    @load luck
+lsr     r1, r0, #1     @put half luck in r1
+add     r0, r0, r1     @add back together.
 mov     r3, #0x60
-ldrh    r1, [r4,r3]   @load hit
-add     r1, r0        @Increase hit by 1.5x Luck.
-strh    r1, [r4,r3]   @store.
+ldrh    r1, [r4,r3]    @load hit
+add     r1, r0         @Increase hit by 1.5x Luck.
+strh    r1, [r4,r3]    @store.
 
 NoSkill:
 pop {r4-r7} 
@@ -42,13 +41,18 @@ bx r0
 .align
 SkillTester:
 @POIN SkillTester
-@POIN FaireIDList
+@WORD AxeFaithID
 
 
 @@Axe Faith
-@@+10 Hit if post-enemy evasion hit is above 40 but below 70.
-
 @@for axe faith Ive settled on axes dont break and hit += luck * 1.5
+
+@@other things said on the matter
+@@+10 Hit if post-enemy evasion hit is above 40 but below 70.
 
 @@but if you follow the Axe Faith
 @@anything over 49 is actually 100
+
+
+
+
