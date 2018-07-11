5bb158af50529ce90a157ef7c010526f1437821d
2WB <naisurte@gmail.com>
2018-07-10 18:25:34 -0700

    Chapter 2 Resources (#39)
    * Add files via upload
    
    * Ch2 & supporting elements
    
    -Armsthrift component of Axefaith: beyond incomplete
    -Completely untested, fidgetting around with how system works.
    
    * [Untested] Axefaith accomodations
    
    Nothing has been tested! I just have to get up now.
    
    -Modifed chapter loot to allow Thief contribute to combat;
    ->Decided to give the other two melee monsterkillers because they're cool things ~'3'~
    -Added newlines to ends of all modified files. A lot of them in some cases. :)
    
    * Fixing stupid
    
    I'm really dumb :)
    
    * Added speaces to be consistent with other text
    
    * Whitespace and case sensitivity fixes.
    
    * Moved all ch2 character text into the character text file.
    
    * Add map changes ID to chapter 2 map.
    
    * Alias capitalizations, fix capitalizations.
    
    * Include Ch2 events.
    
    * Reach assemblable state with events.
    
    * Resolve bugs in event.
    
    * Bigfixes on Axefaith, change skills installation to not needlessly overwrite changes.

--- Engine Hacks/Skill System/Other Skills/armsthriftaxefaith.s
+++ Engine Hacks/Skill System/Other Skills/armsthriftaxefaith.s
@@ -1,12 +1,15 @@
 .thumb
 
-GetItemAfterUse = 0x08016AEC+1
-RollBattleRN    = 0x0802A52C+1
-_ReturnLocation = 0x0802B828+1
+WTYPE_AXE         = 0x02
+GetItemAfterUse   = 0x08016AEC+1
+RollBattleRN      = 0x0802A52C+1
+GetEquippedWeapon = 0x08016B28+1
+GetWeaponType     = 0x08017548+1
+_ReturnLocation   = 0x0802B828+1
 
 LUnitHasSkill      = EALiterals+0x00
 LArmsthriftSkillID = EALiterals+0x04
-LAxefaithSkillID   = EALiterals+0x08
+LAxeFaithSkillID   = EALiterals+0x08
 
 @ Hook from 0802B7F8
 ArmsthriftHook:
@@ -51,6 +54,29 @@ NonMiss:
 NonArmsthrift:
 	@ ACTUAL ARMSTHRIFT CHECK END
 
+	@2wb: begin AxeFaith check
+	mov r0, r5                 @ arg r0 = (Battle) Unit
+	ldr r1, LAxeFaithSkillID   @ arg r1 = Skill Index
+
+	ldr r3, LUnitHasSkill
+	bl BXR3
+
+	cmp r0, #0        @ compare result
+	beq NonAxeFaith   @ goto NonAxeFaith if zero (unit does not have axefaith)
+
+	@ Unit has axefaith
+	@ Are they using an axe?
+	mov r0, r5        @arg r0 = unit
+	ldr r3, =GetEquippedWeapon
+	bl BXR3
+
+	ldr r3, =GetWeaponType
+	bl BXR3
+
+	cmp r0, #WTYPE_AXE @ #0x02 
+	beq End            @ goto End if true (AxeFaith triggers)
+
+NonAxeFaith:
 	mov  r4, #0x48 @ offsetof(BattleUnit.weaponAfter)
 
 	ldrh r0, [r5, r4] @ Load weapon
@@ -79,3 +105,4 @@ BXR3:
 EALiterals:
 	@ POIN SkillTester|1
 	@ WORD ArmsthriftID
+	@ WORD AxeFaithID
